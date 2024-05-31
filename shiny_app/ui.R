9e2afd85d59159537444c5498de0850f3e33bef3
RakeshVarmaAddada <162978924+RakeshVarmaAddada@users.noreply.github.com>
2024-05-31 18:47:22 +0200

    Update ui.R

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -1,1 +1,33 @@
+# shiny_app/ui.R
+
+library(shiny)
+library(leaflet) 
+
+shinyUI(fluidPage(
+  titlePanel("Fire Detection Data Visualizations"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("plotType", "Select Plot Type:",
+                  choices = list("Time Series of Total Fire Detections" = "time_series",
+                                 "Histogram of Fire Intensities" = "histogram",
+                                 "Interactive Map of Fire Detections" = "map"))
+    ),
+    
+    mainPanel(
+      conditionalPanel(
+        condition = "input.plotType == 'time_series'",
+        plotOutput("timeSeriesPlot")
+      ),
+      conditionalPanel(
+        condition = "input.plotType == 'histogram'",
+        plotOutput("histogramPlot")
+      ),
+      conditionalPanel(
+        condition = "input.plotType == 'map'",
+        leafletOutput("mapPlot") 
+      )
+    )
+  )
+))
 
